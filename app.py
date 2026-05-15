ad15cd959bd5d638bdec1dd4daf8a7bb46599e21
ameenmv <shehaaa4@gmail.com>
2026-05-15 18:15:37 +0300

    Fix CORS and Cross-Origin Session Cookies

--- app.py
+++ app.py
@@ -23,11 +23,7 @@ def create_app(config_class=Config):
 
     @app.after_request
     def add_cors_headers(response):
-        origin = request.headers.get('Origin')
-        if origin:
-            response.headers['Access-Control-Allow-Origin'] = origin
-        else:
-            response.headers['Access-Control-Allow-Origin'] = '*'
+        response.headers['Access-Control-Allow-Origin'] = '*'
         response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
         response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
         response.headers['Access-Control-Allow-Credentials'] = 'true'
